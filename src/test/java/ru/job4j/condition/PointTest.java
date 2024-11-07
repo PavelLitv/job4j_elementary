6d03ccabb3ec4daad25cdf79379f721a98fa19b2
Pavel Litvinenko <korvine1980@yandex.ru>
2024-11-07 10:00:46 +0500

    Тестирование Класса Расстояния между Точками [#505280 #520579]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -9,17 +9,17 @@ class PointTest {
     @Test
     void whenPoints38And1820Then19dot2093() {
         int x1 = 3, y1 = 8, x2 = 18, y2 = 20;
-        double expected = 19.209372712298546;
+        double expected = 19.209;
         double actual = Point.distance(x1, y1, x2, y2);
-        assertThat(actual).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(actual).isEqualTo(expected, withPrecision(0.001));
     }
 
     @Test
     void whenPointsMinus180And03Then18dot2482() {
         int x1 = -18, y1 = 0, x2 = 0, y2 = 3;
-        double expected = 18.24828759089466;
+        double expected = 18.248;
         double actual = Point.distance(x1, y1, x2, y2);
-        assertThat(actual).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(actual).isEqualTo(expected, withPrecision(0.001));
     }
 
     @Test
@@ -27,6 +27,6 @@ class PointTest {
         int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
         double expected = 0;
         double actual = Point.distance(x1, y1, x2, y2);
-        assertThat(actual).isEqualTo(expected, withPrecision(0.0001));
+        assertThat(actual).isEqualTo(expected, withPrecision(0.001));
     }
 }
